b5e28739f4db89ce8d6809c8fcf66ba855cf428e
SauloAC <saulo.andrade.costa@gmail.com>
2025-02-17 21:57:04 -0800

    Creating and Drawing in the canvas

---
+++ spriteAnimation.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerSprite = new Image();
+playerSprite.src = "assets/sprites/shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.fillRect(50, 50, 100, 100);
+  // animate player ctx.drawImage(image, source-x, source-y, source-Width, source-Height, dx, dy, dWidth, dHeight);
+  ctx.drawImage(
+    playerSprite,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  // if (frameX < 6) frameX++;
+  // else frameX = 0;
+  requestAnimationFrame(animate);
+}
+
+animate();
