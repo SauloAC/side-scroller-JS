7b1e76d5fcd485418839a101e7f3b2074a8dddfe
SauloAC <saulo.andrade.costa@gmail.com>
2025-02-17 22:55:50 -0800

    Making Animation Dynamic and Testing

--- spriteAnimation.js
+++ spriteAnimation.js
@@ -1,3 +1,9 @@
+let playerState = "idle";
+const dropDown = document.getElementById("animations");
+dropDown.addEventListener("change", function (e) {
+  playerState = e.target.value;
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 600);
@@ -7,21 +13,80 @@ const playerSprite = new Image();
 playerSprite.src = "assets/sprites/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
+// let frameX = 0;
+// let frameY = 0;
 let gameFrame = 0;
-const staggerFrames = 24;
+const staggerFrames = 14;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    location: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.location.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
-  frameX = spriteWidth * position;
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playerState].location.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].location[position].y;
   // animate player ctx.drawImage(image, source-x, source-y, source-Width, source-Height, dx, dy, dWidth, dHeight);
   ctx.drawImage(
     playerSprite,
     frameX,
-    frameY * spriteHeight,
+    frameY,
     spriteWidth,
     spriteHeight,
     0,
